d9b2b31095cd28bbc87950c6c5081061d5606692
Kry765 <krzysztofkleka91@gmail.com>
2024-01-11 22:46:40 +0100

    ...

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,35 +14,64 @@ import { IoMdDoneAll } from 'react-icons/io'
 export default function Skills() {
 	return (
 		<div className='skills' id='Skills'>
-			<h1 className='skills__title'>Umiejętności</h1>
 			<div className='skills__flex'>
 				<div className='skills__description'>
-					<div>
-						<span>
-							<IoMdDoneAll />
-						</span>
-						<p>Na co dzień pracuje z technologiami Front-Endowymi takimi jak React oraz JavaScript.</p>
-					</div>
-					<div>
-						<p>
-							<span>
-								<IoMdDoneAll />
-							</span>
-							Obecnie jestem w trakcie nauki Docker’a oraz dodatkowego języka relacyjnych baz danych PostgreSQL, za
-							pomocą którego będę mógł tworzyć w przyszłości profesjonalne sklepy internetowe.
-						</p>
+					<h1 className='skills__title'>Umiejętności</h1>
+					<div className='skills__icon-position'>
+						<div className='flex-row'>
+							<div>
+								<IoMdDoneAll className='skills__icon-description' />
+							</div>
+							<p>
+								Obecnie jestem w trakcie nauki Docker’a oraz dodatkowego języka relacyjnych baz danych PostgreSQL, za
+								pomocą którego będę mógł tworzyć w przyszłości profesjonalne sklepy internetowe.
+							</p>
+						</div>
+						<div className='flex-row'>
+							<div>
+								<IoMdDoneAll className='skills__icon-description' />
+							</div>
+							<p>Na co dzień pracuje z technologiami Front-Endowymi takimi jak React oraz JavaScript.</p>
+						</div>
 					</div>
 				</div>
 				<div className='skills__icon-skills'>
-					<DiPostgresql className='skills__icon-skill' />
-					<AiFillHtml5 className='skills__icon-skill' />
-					<FaSass className='skills__icon-skill' />
-					<IoLogoCss3 className='skills__icon-skill' />
-					<DiJavascript className='skills__icon-skill' />
-					<SiReact className='skills__icon-skill' />
-					<FaNodeJs className='skills__icon-skill' />
-					<AiFillGithub className='skills__icon-skill' />
-					<SiBem className='skills__icon-skill' />
+					<div>
+						<DiPostgresql className='skills__icon-skill' />
+						<p className='skills__name'>postgresql</p>
+					</div>
+					<div>
+						<AiFillHtml5 className='skills__icon-skill' />
+						<p className='skills__name'>html</p>
+					</div>
+					<div>
+						<FaSass className='skills__icon-skill' />
+						<p className='skills__name'>scss</p>
+					</div>
+					<div>
+						<IoLogoCss3 className='skills__icon-skill' />
+						<p className='skills__name'>css</p>
+					</div>
+					<div>
+						<DiJavascript className='skills__icon-skill' />
+						<p className='skills__name'>js</p>
+					</div>
+					<div>
+						<SiReact className='skills__icon-skill' />
+						<p className='skills__name'>react</p>
+					</div>
+					<div>
+						<FaNodeJs className='skills__icon-skill' />
+						<p className='skills__name'>node.js</p>
+					</div>
+					<div>
+						<AiFillGithub className='skills__icon-skill' />
+						<p className='skills__name'>github</p>
+					</div>
+					<div>
+						<SiBem className='skills__icon-skill' />
+						<p className='skills__name'>BEM</p>
+					</div>
 				</div>
 			</div>
 		</div>
