aaa64f8eaaae6306c4c92fa41129af9a7e9b248d
Kry765 <krzysztofkleka91@gmail.com>
2024-03-08 22:52:34 +0100

    remove all items

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React from 'react'
-import Menu from './components/Menu'
-import Header from './components/Header'
-import Aboutme from './components/Aboutme'
-import Skills from './components/Skills'
-import Offerts from './components/Offerts'
-// import Project from './components/Project'
-// import Footer from './components/Footer'
 
 function App() {
 	return (
 		<div className='App'>
-			<nav>
-				<Menu id='Menu' />
-
-				<header>
-					<Header id='Header' />
-				</header>
-			</nav>
-			<main>
-				<section>
-					<Aboutme id='Aboutme' />
-				</section>
-				<section>
-					<Skills id='Skills' />
-				</section>
-				<section>
-					<Offerts id='Offerts' />
-				</section>
-				{/* <section> */}
-				{/* <Project id='Project' /> */}
-				{/* </section> */}
-			</main>
-			{/* <footer>
-				<Footer />
-			</footer> */}
+			<div>app</div>
 		</div>
 	)
 }
