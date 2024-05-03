7cf587f14fa7c7356c288e4d3d6e65f23ebace53
Kry765 <krzysztofkleka91@gmail.com>
2024-05-03 21:40:15 +0200

    ...

--- src/components/HeaderComponents/HeaderBtn.jsx
+++ src/components/HeaderComponents/HeaderBtn.jsx
@@ -1,6 +1,6 @@
 export const HeaderBtn = () => {
   return (
-    <button className="animate-fade-right hover:cursor-pointer hover:bg-[#176B87] border-none bg-[#401866] rounded-2xl	h-[50px] w-[250px] font-bold uppercase transition duration-300">
+    <button className="animate-fade-right hover:cursor-pointer hover:bg-[#176B87] border-none bg-[#873317] rounded-2xl	h-[50px] w-[250px] font-bold uppercase transition duration-300">
       <a href="#aboutme" className="text-white">
         Let's Go
       </a>
