43a5e4cfeca221af9b5f13f1eb4850d1b976cd51
Kry765 <krzysztofkleka91@gmail.com>
2024-05-09 23:04:01 +0200

    repair link

--- src/components/AboutComponents/RenderAboutDescription.jsx
+++ src/components/AboutComponents/RenderAboutDescription.jsx
@@ -3,10 +3,10 @@ import { AboutDescriptionsText, AboutMeIcon } from "../../data/data";
 export const RenderAboutDescriptions = () => {
   return AboutDescriptionsText.map((AboutDescriptionText, index) => (
     <div
-      className="flex border border-solid w-1/6 border-white p-12 rounded-lg justify-start items-center text-center flex-col m-8 text-xl md:w-full"
+      className="flex border border-solid w-1/6  border-white p-12 rounded-lg justify-start items-center text-center flex-col m-8 text-xl md:w-full"
       key={index}
     >
-      <p className="text-[#019760] my-8">{AboutMeIcon[index]}</p>
+      <p className="text-[#eee] my-8">{AboutMeIcon[index]}</p>
       <p>{AboutDescriptionText}</p>
     </div>
   ));
--- src/components/HeaderComponents/HeaderBtn.jsx
+++ src/components/HeaderComponents/HeaderBtn.jsx
@@ -1,10 +1,10 @@
 export const HeaderBtn = () => {
   return (
-    <button className="animate-fade-right hover:cursor-pointer mx-8 hover:bg-[#176B87] border-none bg-[#019760] rounded-2xl h-[50px] w-[250px] font-bold uppercase transition duration-300">
-      <a href="#aboutme" className="text-white">
+    <a href="#contact" className="text-white">
+      <button className="animate-fade-right hover:cursor-pointer mx-8 hover:bg-[#eee] hover:text-[#000] border-none bg-[#019760] rounded-2xl h-[50px] w-[250px] font-bold uppercase transition duration-300">
         Contact me
-      </a>
-    </button>
+      </button>
+    </a>
   );
 };
 
